94c9f8be5a79f95fe6926d04e0ed942c115372fa
Alan Kavanagh <alan.kavanagh@gm.com>
2019-02-02 22:27:41 +0000

    Created User and Security service implementations

---
+++ src/main/java/com/deloitte/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,54 @@
+package com.deloitte.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.deloitte.Application;
+import com.deloitte.service.SecurityService;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+	private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public String findLoggedInUsername() {
+    	logger.debug("getting logged in username");
+
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getUsername();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void autologin(String username, String password) {
+    	logger.debug("trying to autologin");
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            logger.debug(String.format("auto login %s successful!", username));
+        }
+    }
+}
